b47089f56e769a60598fdbe28c617eb0dfca9cc3
Dyson <dysonkui@gmail.com>
2025-04-14 12:26:33 +0400

    Added grabbable object and changed player prefab on warehouse scene

--- Enhanced/Assets/_Dyson/Player/Scripts/ObjectGrabbable.cs
+++ Enhanced/Assets/_Dyson/Player/Scripts/ObjectGrabbable.cs
@@ -3,34 +3,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectGrabbable : MonoBehaviour
+namespace Enhanced.Dyson.Player
 {
-    private Rigidbody rb;
-    private Transform objectGrabPointTransform;
-    float lerpSpeed = 10f;
-    private void Awake()
+    public class ObjectGrabbable : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody>();
-    }
+        private Rigidbody rb;
+        private Transform objectGrabPointTransform;
+        float lerpSpeed = 10f;
 
-    public void Grab(Transform objectGrabPointTransform)
-    {
-        this.objectGrabPointTransform = objectGrabPointTransform;
-        rb.useGravity = false;
-    }
-    
-    public void Drop()
-    {
-        objectGrabPointTransform = null;
-        rb.useGravity = true;
-    }
-
-    private void FixedUpdate()
-    {
-        if (objectGrabPointTransform != null)
+        private void Awake()
         {
-            Vector3 newPosition = Vector3.Lerp(transform.position, objectGrabPointTransform.position, Time.deltaTime * lerpSpeed);
-            rb.MovePosition(newPosition);
+            rb = GetComponent<Rigidbody>();
+        }
+
+        public void Grab(Transform objectGrabPointTransform)
+        {
+            this.objectGrabPointTransform = objectGrabPointTransform;
+            rb.useGravity = false;
+        }
+
+        public void Drop()
+        {
+            objectGrabPointTransform = null;
+            rb.useGravity = true;
+        }
+
+        private void FixedUpdate()
+        {
+            if (objectGrabPointTransform != null)
+            {
+                Vector3 newPosition = Vector3.Lerp(transform.position, objectGrabPointTransform.position,
+                    Time.deltaTime * lerpSpeed);
+                rb.MovePosition(newPosition);
+            }
         }
     }
 }
