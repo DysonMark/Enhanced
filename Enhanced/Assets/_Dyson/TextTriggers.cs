ae85ac95a042bb7c221e172addf489fb46d844ef
Dyson <dysonkui@gmail.com>
2025-04-19 14:53:29 +0400

    Setup text triggers when you enter an alley

--- Enhanced/Assets/_Dyson/TextTriggers.cs
+++ Enhanced/Assets/_Dyson/TextTriggers.cs
@@ -6,12 +6,43 @@ using UnityEngine;
 
 public class TextTriggers : MonoBehaviour
 {
-    [SerializeField] private GameObject alleyOne; 
+    [SerializeField] private GameObject alleyOne;
+    [SerializeField] private GameObject alleyTwo;
+    [SerializeField] private GameObject alleyThree;
     private void OnTriggerEnter(Collider other)
+    {
+        if (this.gameObject.name == "Trigger_1" || this.gameObject.name == "Trigger_1 (1)")
+        {
+            if (other.tag == "Player")
+            {
+                alleyOne.SetActive(true);
+            }
+        }
+        
+        if (this.gameObject.name == "Trigger_2" || this.gameObject.name == "Trigger_2 (1)")
+        {
+            if (other.tag == "Player")
+            {
+                alleyTwo.SetActive(true);
+            }
+        }
+        
+        if (this.gameObject.name == "Trigger_3" || this.gameObject.name == "Trigger_3 (1)")
+        {
+            if (other.tag == "Player")
+            {
+                alleyThree.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player")
         {
-            alleyOne.SetActive(true);
+            alleyOne.SetActive(false);
+            alleyTwo.SetActive(false);
+            alleyThree.SetActive(false); 
         }
     }
 
